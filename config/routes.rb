bc0b1b6416bc761fd6d0219f0b3d23b9cdd9d9ec
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-21 04:53:52 +0000

    [Add]book_commentsコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/home/about" => "homes#about", as: "about"
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,] do
+    resources :books_comments, only: [:create :destroy]
   resources :users, only: [:index, :show, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
