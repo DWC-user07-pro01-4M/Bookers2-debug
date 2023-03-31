419234ed2e6d396d96565979b573071a7d22edcb
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-31 03:57:52 +0000

    [Add・Fix]記述の追加と修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,22 @@
 class RelationshipsController < ApplicationController
-  def followings
+  # フォローする時
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
   end
-
-  def follwers
+  # フォローを外す時
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+  # フォロー一覧
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  # フォロワー一覧
+  def follwer
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
 end
