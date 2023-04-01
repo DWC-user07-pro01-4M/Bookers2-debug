e36d554ed5ffb378c06b1ade94b59451e6bf50d5
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-01 04:58:13 +0000

    [Add]searchのルーティングと部分テンプレート作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   end
 
   # 検索機能
-
+  get "search" => "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
