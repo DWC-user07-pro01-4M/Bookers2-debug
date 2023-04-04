bfea914c5a2a89920b00117c94beaaeb9044664c
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-04 06:02:35 +0000

    [fix]コメント投稿・削除反映されない不具合を修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     @comment.save
-    render 'index'
+    # render 'index'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    render 'index'
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    # render 'index'
   end
 
   private
