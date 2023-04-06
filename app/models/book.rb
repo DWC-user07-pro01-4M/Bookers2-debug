85f4e98b1e4f216c08609ec33e25b5edac9f3897
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-06 04:25:56 +0000

    [Start]応用課題７b記録アプリの機能・book.rbをにスコープ機能をつける

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,4 +22,10 @@ class Book < ApplicationRecord
       Book.where('title LIKE?', '%' + content + '%')
     end
   end
+  # 投稿数を日付別に表示する方法とテーブルボードの作り方
+  # 本の投稿数を数える・モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
+  # created_todayがスコープ名。Time.zone.now.all_dayで１日を表す
+  scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
+  # created_yesterdayがスコープ名。1.day.agoで昨日を表す
+  scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
 end
