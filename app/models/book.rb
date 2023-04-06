a84682ea215a68321e82dea38471cc3e73446e17
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-06 05:09:39 +0000

    [Add]book.rbに今週と先週の投稿を呼び出す記述を追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,10 +22,14 @@ class Book < ApplicationRecord
       Book.where('title LIKE?', '%' + content + '%')
     end
   end
-  # 投稿数を日付別に表示する方法とテーブルボードの作り方
-  # 本の投稿数を数える・モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
+  # 投稿数を日付別に表示する
+  # モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
   # created_todayがスコープ名。Time.zone.now.all_dayで１日を表す
   scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
   # created_yesterdayがスコープ名。1.day.agoで昨日を表す
   scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
+  # 今週
+  scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
+  # 先週
+  scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
 end
