4649884f2261d60a42db63c2ebc2a4f52af7776f
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-06 08:09:10 +0000

    [Add/Fix]過去７日間分のデータをワンライングラフ表示モデルと共通化ファイル作成とリファクタリングをして記述を省略

--- app/models/book.rb
+++ app/models/book.rb
@@ -32,4 +32,9 @@ class Book < ApplicationRecord
   scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
   # 先週
   scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
+  # 過去 7 日間分、それぞれの投稿数を一覧表示・リファクタリング
+  scope :created_days_ago, -> (n) {where(created_at: n.days.ago.all_day)}
+  def self.past_week_count
+    (1..7).map { |n| created_days_ago(n).count }.reverse
+  end
 end
