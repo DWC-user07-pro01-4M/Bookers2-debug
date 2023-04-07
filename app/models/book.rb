fcfb8cd310cf3f55206b9fe2ca55b8be99d566f7
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-07 04:30:15 +0000

    [Finish]応用課題8b

--- app/models/book.rb
+++ app/models/book.rb
@@ -22,21 +22,23 @@ class Book < ApplicationRecord
       Book.where('title LIKE?', '%' + content + '%')
     end
   end
+
+  # 応用課題７B・ここから
   # 投稿数を日付別に表示する
   # モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
   # created_todayがスコープ名。Time.zone.now.all_dayで１日を表す
-  scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
-  # created_yesterdayがスコープ名。1.day.agoで昨日を表す
-  scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
-  # 過去 7 日間分、それぞれの投稿数を一覧表示
-  scope :created_2days, -> {where(created_at: 2.days.ago.all_day)}
-  scope :created_3days, -> {where(created_at: 3.days.ago.all_day)}
-  scope :created_4days, -> {where(created_at: 4.days.ago.all_day)}
-  scope :created_5days, -> {where(created_at: 5.days.ago.all_day)}
-  scope :created_6days, -> {where(created_at: 6.days.ago.all_day)}
-  # 今週
-  scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
-  # 先週
-  scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
+  # scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
+  # # created_yesterdayがスコープ名。1.day.agoで昨日を表す
+  # scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
+  # # 今週
+  # scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
+  # # 先週
+  # scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
+  # 応用課題７B・ここまで
 
+  # 過去 7 日間分、それぞれの投稿数を一覧表示
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+  def self.past_week_count
+    (1..6).map { |n| created_days_ago(n).count }.reverse
+  end
 end
