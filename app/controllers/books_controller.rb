d7b1bd9a3af68edea122a0fcffa863cf7817f3fc
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-09 12:02:23 +0000

    [Add]応用課題９Abooksコントローラのshowに記述を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,12 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
+    # 応用課題９A・ここから
+    @book_detail = Book.find(params[:id])
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.read_counts.create(book_id: @book_detail.id)
+    end
+    # 応用課題９A・ここまで
   end
 
   def index
