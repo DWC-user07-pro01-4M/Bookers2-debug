c3ffae397f3e60f5a268e533b6411655da92dafa
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-09 07:15:13 +0000

    [Add]応用課題８Achatsのルーティングを指定

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
-
   # 検索機能
   # get "search" => "searches#search"
   get '/search', to: 'searches#search'
-
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy,] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
@@ -17,6 +15,11 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+
+    # 応用課題８A・ここから
+      resources :chats, only: [:show, :create]
+    # 応用課題８A・ここまで
+
     # 応用課題９B・ユーザーの詳細ページに検索欄を表示させたいので、ネストさせる・ここから
     # get 'search', to: 'users#search'#私の記述
     # 応用課題９B・ここまで
