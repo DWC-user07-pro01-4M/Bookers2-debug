1fdece4787da9de0f2d09be07e1200769b30d9dc
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-10 14:01:57 +0000

    [Fix]各Viewの記述を変更

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,8 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   validates :name, presence: true
   validates :introduction, presence: true
-  attachment :image, destroy: false
+  has_one_attached :image
+  def get_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end
