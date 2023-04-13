ddccdd5511bc96bdb9daf58b70a2c29dc41fc2e5
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-13 07:16:06 +0000

    [Fix]応用課題８Cviewsを修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,29 +1,29 @@
 class Group < ApplicationRecord
   # 応用課題７C８C９Cで使用
   # 私の記述
-#   has_many :group_users
-#   has_many :users, through: :group_users
-#   validates :name, presence: true
-#   validates :introduction, presence: true
-#   has_one_attached :image
-#   def get_image
-#     (image.attached?) ? image : 'no_image.jpg'
-#   end
-# end
-
-# 模範回答
-  has_one_attached :image
-  belongs_to :owner, class_name: 'User'
-  has_many :group_users, dependent: :destroy
-
+  has_many :group_users
+  has_many :users, through: :group_users
   validates :name, presence: true
   validates :introduction, presence: true
-
+  has_one_attached :image
   def get_image
     (image.attached?) ? image : 'no_image.jpg'
   end
-
-  def is_owned_by?(user)
-    owner.id == user.id
-  end
 end
+
+# 模範回答
+#   has_one_attached :image
+#   belongs_to :owner, class_name: 'User'
+#   has_many :group_users, dependent: :destroy
+
+#   validates :name, presence: true
+#   validates :introduction, presence: true
+
+#   def get_image
+#     (image.attached?) ? image : 'no_image.jpg'
+#   end
+
+#   def is_owned_by?(user)
+#     owner.id == user.id
+#   end
+# end
