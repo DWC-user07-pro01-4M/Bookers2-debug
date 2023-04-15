28d6c719e5585371593b47d3596613da9d4d4147
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-15 12:42:13 +0000

    [Add/Fix]応用課題７D・星評価レビューの為のカラム追加記述の変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,32 +1,55 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// import "jquery"
+// import "popper.js"
+// import "bootstrap"
+// import '@fortawesome/fontawesome-free/js/all';
+// import "../stylesheets/application"
+
+// 応用課題７B
+// Javascriptのライブラリ読み込み
+// import Chart from "chart.js/auto";
+// global.Chart = Chart;
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+// 応用課題７D
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import jQuery from "jquery"
-import "jquery"
+import jQuery from "jquery"
+import raty from 'raty-js'
 import "popper.js"
 import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
-// import Raty from "raty.js"
-// window.raty = function(elem,opt) {
-//   let raty =  new Raty(elem,opt)
-//   raty.init();
-//   return raty;
-// }
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-// Javascriptのライブラリ読み込み
-// import Chart from "chart.js/auto";
-// global.Chart = Chart;
+global.$ = jQuery;
+window.$ = jQuery;
 
-// 応用課題７Dのために追加
-window.$ = window.jQuery = require('jquery');
-// require('packs/raty')
-// global.$ = jQuery;
-// window.$ = jQuery;
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
