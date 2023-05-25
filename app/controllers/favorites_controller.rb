594b3fa4a8afdefa3fdcf64e84c3f61bbc5704d5
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-25 10:41:19 +0000

    Fix 課題７Dの実装のため不要な記述は削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,8 @@
 class FavoritesController < ApplicationController
-
-  def index
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort_by {|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
-    @book = Book.new
-  end
-
   def create
     book = Book.find(params[:book_id])
     @favorite = current_user.favorites.new(book_id: book.id)
     @favorite.save
-    # redirect_to request.referer
     render 'replace_btn'
   end
 
@@ -22,7 +10,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     @favorite = current_user.favorites.find_by(book_id: book.id)
     @favorite.destroy
-    # redirect_to request.referer
     render 'replace_btn'
   end
 end
