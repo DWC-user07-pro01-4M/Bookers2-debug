f381364390070e5a6ff5d2ba75b16c49382bd089
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-25 10:18:41 +0000

    Fix 投稿時のエラーを解消中

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  root to: "homes#top"
-  get "home/about" => "homes#about"
-  # 検索機能
-  # get "search" => "searches#search"
-  get '/search', to: 'searches#search'
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy,] do
-    resource :favorites, only: [:create, :destroy]
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update] do
+  resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-    # 応用課題９B・ユーザーの詳細ページに検索欄を表示させたいので、ネストさせる・ここから
-    # get 'search', to: 'users#search'#私の記述
-    # 応用課題９B・ここまで
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
   end
-
-  # 応用課題８A・ここから
-  # resources :chats, only: [:show, :create]
-  # 応用課題８A・ここまで
-
-  # 応用課題７C８C９Cここから
-  # resources :groups, only: [:new, :index, :show, :edit, :create, :update, :destroy]
-  # resources :groups, except: [:destroy] #省略形
-  # resources :groups do
-  #   get "join" => "groups#join"
-  # end
-  # 模範回答
-   resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
-    resource :group_users, only: [:create, :destroy]
-  end
-  # 応用課題７C８C９Cここまで
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search', to: 'searches#search'
 end
