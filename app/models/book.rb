1f17e0c6e1470203f2ac3886bd98e9888527d872
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-26 06:03:33 +0000

    Fix 応用課題C９のために不要な記述は削除

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,26 +3,6 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  # 応用課題７A・本の投稿一覧ページで、過去一週間でいいねの合計カウントが多い順に投稿を表示・ここから
-  # 私の記述
-  # has_many :favorited_users, through: :favorites, source: :user
-
-  # 模範解答
-  # BookモデルがFavoriteモデルをhas_manyで関連付けている際、特定の条件に一致するFavoriteのレコードのみを抽出するためのスコープを定義
-  # :week_favoritesという名前のスコープを定義,このスコープは、class_nameオプションで指定されたFavoriteモデルに対して定義
-  # whereメソッドによって、条件式created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)に一致するFavoriteのレコードを取得
-  # Timeクラスのcurrentメソッドで現在の日時を取得し、at_end_of_dayメソッドでその日の終わりの時刻を取得
-  # それから、6.dayという期間を引いて、6日前の時刻を取得
-  # at_beginning_of_dayメソッドでその日の始まりの時刻を取得
-  # これによって、現在から6日前の1週間分の期間を表す範囲が設定
-  # 最後に、この範囲に含まれるcreated_at属性の値を持つFavoriteのレコードを取得するため、範囲演算子..を使用して条件式を定義
-  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
-  # 応用課題７A・ここまで
-
-  # 応用課題９A・ここから
-  has_many :read_counts, dependent: :destroy
-  # 応用課題９A・ここまで
-
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
@@ -43,24 +23,4 @@ class Book < ApplicationRecord
     end
   end
 
-  # 応用課題７B・ここから
-  # 投稿数を日付別に表示する
-  # モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
-  # created_todayがスコープ名。Time.zone.now.all_dayで１日を表す
-  # scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
-  # # created_yesterdayがスコープ名。1.day.agoで昨日を表す
-  # scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
-  # # 今週
-  # scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
-  # # 先週
-  # scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
-  # 応用課題７B・ここまで
-
-  # 応用課題８Bここから
-  # 過去 7 日間分、それぞれの投稿数を一覧表示
-  # scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
-  # def self.past_week_count
-  #   (1..6).map { |n| created_days_ago(n).count }.reverse
-  # end
-  # 応用課題８Bここまで
 end
