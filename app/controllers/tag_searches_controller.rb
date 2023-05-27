2602139ddf0d32b22036e9826956fd659ce458ff
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-27 06:32:03 +0000

    Add タグコントローラに定義を追加

--- app/controllers/tag_searches_controller.rb
+++ app/controllers/tag_searches_controller.rb
@@ -1,2 +1,9 @@
 class TagSearchesController < ApplicationController
+  # 応用課題D９から追加
+  def search
+    @model = Book
+    @word = params[:word]
+    @books = Book.where("tag LIKE?", "%#{@word}%")
+    render "searches/result"
+  end
 end
