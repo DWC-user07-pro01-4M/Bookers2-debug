18b5ad0ca37d9478eff8c7c8f7582729f21937e3
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-28 01:03:48 +0000

    Fix 不要な記述は削除

--- app/models/book.rb
+++ app/models/book.rb
@@ -30,7 +30,6 @@ class Book < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
-  # 検索方法分岐
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -43,24 +42,4 @@ class Book < ApplicationRecord
     end
   end
 
-  # 応用課題７B・ここから
-  # 投稿数を日付別に表示する
-  # モデルのスコープ機能・scope :スコープの名前, -> { 条件式 }
-  # created_todayがスコープ名。Time.zone.now.all_dayで１日を表す
-  # scope :created_today, -> {where(created_at: Time.zone.now.all_day)}
-  # # created_yesterdayがスコープ名。1.day.agoで昨日を表す
-  # scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
-  # # 今週
-  # scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
-  # # 先週
-  # scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day)}
-  # 応用課題７B・ここまで
-
-  # 応用課題８Bここから
-  # 過去 7 日間分、それぞれの投稿数を一覧表示
-  # scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
-  # def self.past_week_count
-  #   (1..6).map { |n| created_days_ago(n).count }.reverse
-  # end
-  # 応用課題８Bここまで
 end
